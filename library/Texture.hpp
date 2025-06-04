39ba02dfa96e984d346987aa4c0a07ccb86d23e8
Prabhas Kurapati <prabhask@berkeley.edu>
2025-06-04 14:45:58 -0700

    implement base texture type

---
+++ library/Texture.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Vec3Types.hpp"
+
+// Interface for all surface textures, defining the Color of the material at a
+// specific point.
+class Texture {
+public:
+  virtual ~Texture() = default;
+
+  // Used by material classes to define the surface Color of the material at
+  // that specific point.
+  virtual Color value(double u, double v, const Point3 &p) const = 0;
+};
---
+++ library/TextureTypes.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <memory>
+
+class Texture; // From Texture.hpp.
+using TexturePtr = std::shared_ptr<Texture>;
