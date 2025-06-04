73580fe43d49f28e0ffd5053d7c42451564681d3
Prabhas Kurapati <prabhask@berkeley.edu>
2025-06-04 14:46:49 -0700

    implement checker texture

---
+++ src/scene/textures/CheckerTexture.cpp
@@ -0,0 +1,27 @@
+#include "CheckerTexture.hpp"
+#include "SolidColorTexture.hpp"
+#include <Vec3.hpp>
+
+CheckerTexture::CheckerTexture(double scale, TexturePtr even_texture,
+                               TexturePtr odd_texture)
+    : m_scale(scale), m_even_texture(even_texture), m_odd_texture(odd_texture) {
+}
+
+CheckerTexture::CheckerTexture(double scale, const Color &c1, const Color &c2)
+    : CheckerTexture(scale, std::make_shared<SolidColorTexture>(c1),
+                     std::make_shared<SolidColorTexture>(c2)) {}
+
+Color CheckerTexture::value(double u, double v, const Point3 &p) const {
+  double inv_scale = 1.0 / m_scale;
+
+  // Scale the point down to the texture's scale, in all three dimensions.
+  int x_index = int(std::floor(inv_scale * p.x()));
+  int y_index = int(std::floor(inv_scale * p.y()));
+  int z_index = int(std::floor(inv_scale * p.z()));
+
+  // Use this to determine which checker space we're on.
+  bool is_even = (x_index + y_index + z_index) % 2 == 0;
+
+  return is_even ? m_even_texture->value(u, v, p)
+                 : m_odd_texture->value(u, v, p);
+}
---
+++ src/scene/textures/CheckerTexture.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Texture.hpp>
+#include <TextureTypes.hpp>
+
+class CheckerTexture : public Texture {
+public:
+  CheckerTexture(double scale, TexturePtr even_texture, TexturePtr odd_texture);
+
+  CheckerTexture(double scale, const Color &c1, const Color &c2);
+
+  Color value(double u, double v, const Point3 &p) const override;
+
+private:
+  double m_scale;
+  TexturePtr m_even_texture;
+  TexturePtr m_odd_texture;
+};
