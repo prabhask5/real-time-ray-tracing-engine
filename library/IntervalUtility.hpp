d2e405f81ca4a63891e82451992792fda0de5794
Prabhas Kurapati <prabhask@berkeley.edu>
2025-06-09 11:27:59 -0700

    update interval utility to fix compiler errors

--- library/IntervalUtility.hpp
+++ library/IntervalUtility.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <Interval.hpp>
+#include "Interval.hpp"
 
-Interval operator+(const Interval &ival, double displacement) {
+inline Interval operator+(const Interval &ival, double displacement) {
   return Interval(ival.min() + displacement, ival.max() + displacement);
 }
 
-Interval operator+(double displacement, const Interval &ival) {
+inline Interval operator+(double displacement, const Interval &ival) {
   return ival + displacement;
 }
