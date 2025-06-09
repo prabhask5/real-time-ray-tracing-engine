90f06027e68afd93b6c3b5c0e6b255438865e4ed
Prabhas Kurapati <prabhask@berkeley.edu>
2025-06-09 11:29:42 -0700

    update hit record to support textures

--- src/core/HitRecord.hpp
+++ src/core/HitRecord.hpp
@@ -27,6 +27,13 @@ struct HitRecord {
   // ray). Back face = the ray hits from inside (normal faces same way as ray).
   bool frontFace;
 
+  // 2D coordinates used to map a 2D texture image onto a 3D surface. When a ray
+  // hits a surface (like a sphere or triangle), the u and v values tell the
+  // renderer which part of the texture to apply at that point. This would be
+  // able to determine what the texture of the object looks like at the position
+  // the ray hit the object.
+  double u, v;
+
   // Sets the hit record normal vector.
   // NOTE: the parameter `outward_normal` is assumed to have unit length.
   void set_face_normal(const Ray &ray, const Vec3 &outward_normal) {
